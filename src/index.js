c183c3d2a4daf9274040802369f563eed8d738ba
tania <masiukevich@gmail.com>
2018-09-16 13:34:12 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function сheck(str, bracketsConfig) {
+    var start = '';
+    var end = '';
+    var s = [];
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        start += bracketsConfig[i][0];
+        end += bracketsConfig[i][1];
+    };
+    for (var j = 0; j < str.length; j++) {
+        if (start.includes(str[j]) && end.includes(str[j])){
+            console.log('availeble in both parts');
+            if (!s.includes(str[j])) {
+                s.push(str[j]);
+            } else {
+                s.pop();
+            }
+            continue;
+        }
+        if (start.includes(str[j])) {
+            s.push(str[j]);
+            continue;
+        }
+        if (!end.includes(str[j])) {
+            return false;
+        }
+        //check if array is empty or 1 element
+        if (!s.length) {
+            return false;
+        }
+
+        var popitem = s.pop();
+        var indexForSearch = end.indexOf(str[j]);
+        var pairValue = start[indexForSearch];
+        //find pair
+
+        if (pairValue != popitem) {
+            return false;
+        }
+
+    }
+    return (!s.length);
 }
+
